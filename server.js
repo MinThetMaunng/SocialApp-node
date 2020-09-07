f6bc08ffd235cf2a797e3a680d080de50648b66f
MinThetMaung <minthetmaung96@gmail.com>
2020-09-07 20:49:16 +0630

    Testing for heroku

--- server.js
+++ server.js
@@ -5,4 +5,5 @@ const app = require('./config/app')(express())
 const logs = require('./helpers/logs')
 const port = process.env.PORT || 3000
 
+app.get('/', (req,res) => res.send('<h1>Hello World</h1>') )
 app.listen(port, () => console.log(logs.ServerRunning))
