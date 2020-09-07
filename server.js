6f6ef75e3ec3b5fa6205d414122ed54f594100f9
MinThetMaung <minthetmaung96@gmail.com>
2020-09-07 22:00:29 +0630

    remove test routes

--- server.js
+++ server.js
@@ -5,7 +5,6 @@ const app = require('./config/app')(express())
 const logs = require('./helpers/logs')
 const port = process.env.PORT || 3000
 
-app.get('/', (req,res) => res.send('<h1>Hello World</h1>') )
 app.listen(port, () => {
     console.log(logs.serverRunning)
     console.log(logs.environment)
