29bb21af1e443376a8786fba1ffcbcc07f43eaf6
MinThetMaung <minthetmaung96@gmail.com>
2020-09-28 16:48:27 +0630

    add status in response data from jwt middleware

--- middlewares/authenticate.js
+++ middlewares/authenticate.js
@@ -4,13 +4,13 @@ const authenticate = async (req, res, next) => {
     try {
         const token = req.headers.authorization
         if(!token) {
-            return res.status(403).json({data: null, message: "Token must be provided."})
+            return res.status(403).json({status: 403, data: null, message: "Token must be provided."})
         }
         const decode = await jwt.verify(token, process.env.JWT_SECRET)
         req.user = decode
         next()
     } catch(err) {
-        return res.status(403).json({data: null, message: "Token expired!"})
+        return res.status(403).json({status: 403, data: null, message: "Token expired!"})
     }
 }
 
